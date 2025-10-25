33afcd4357f2a816f48ee20d524bd7d30432a34b
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-25 12:36:48 +0200

    no message

---
+++ Notes Watch App/Model/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  Notes Watch App
+//
+//  Created by Ghenadie Isacenco on 25/10/2025.
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+    let id: UUID
+    let text: String
+}
