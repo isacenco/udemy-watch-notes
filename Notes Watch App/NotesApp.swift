b74d019ae3fb20430b5c06614546486f97bdc35a
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-25 12:11:57 +0200

    Project Setup

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Ghenadie Isacenco on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
